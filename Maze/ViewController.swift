f6d64aa396409175977f4af54d8f43ae7f06eee6
天毛 瞬 <shuntemmo@icloud.com>
2016-07-01 20:35:15 +0900

    first commit

--- Maze/ViewController.swift
+++ Maze/ViewController.swift
@@ -9,15 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let screenSize = UIScreen.mainScreen().bounds.size
+    let maze = [
+    [1, 0, 0, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0],
+    [3, 0, 1, 0, 1, 0],
+    [1, 1, 1, 0, 0, 0],
+    [1, 0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 0, 0],
+    [0, 1, 1, 0, 1, 0],
+    [0, 0, 0, 0, 1, 1],
+    [0, 1, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 2]
+        
+    ]
+    
+    var goalView: UIView!
+    var starView:UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let cellWidth = screenSize.width / CGFloat(maze[0].count) // 6
+        let cellHeight = screenSize.height / CGFloat(maze.count)//10
+        
+        let cellOffsetX = screenSize.width / CGFloat(maze[0].count * 2)
+        let cellOffsetY = screenSize.height / CGFloat(maze.count * 2)
+        
+        for y in 0 ..< maze.count {
+            for _ in 0 ..< maze[y].count {
+                switch maze[y][x] {
+                    case 1:
+                    let wallView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX,
+                                              offsetY: cellOffsetY)
+                    wallView.backgroundColor = UIColor.blackColor()
+                    view.addSubview(wallView)
+                    case 2:
+                    startView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX,
+                                              offsetY: cellOffsetY)
+                     startView.backgroundColor = UIColor.greenColor()
+                    self.view.addSubview(startView)
+                    case 3:
+                    goalView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX,
+                                              offsetY: cellOffsetY)
+                    goalView.backgroundColor = UIColor.redColor()
+                    self.view.addSubview(goalView)
+                default:
+                    break
+
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func createView(x x:Int, y: Int, width: CGFloat, height: CGFloat, offsetX: CGFloat = 0, offsetY: CGFloat = 0) ->
+        UIView {
+            let rect = CGRect(x: 0, y: 0, width: width, height: height)
+            let view = UIView(frame: rect)
+            
+            let center = CGPoint(
+                x: offsetX + width * CGFloat(x),
+                y: offsetY + height * CGFloat(y)
+            )
+            
+            view.center = center
+            return view
     }
 
 
